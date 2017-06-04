b459df581c22eff17061befc67fcd7f75094f734
Auroraka <yangtianlong111@gmail.com>
2017-06-05 00:00:26 +0800

    add more function

--- js/api.js
+++ js/api.js
@@ -6,34 +6,6 @@
 // ---------------- API for fore-end use ----------------
 // ---------------- If need any new api, please write it here, backend will fill it soon ------------
 
-
-function getInstructions() {
-    let instructions = new Array(insQueue.commands.length);
-    for (var i in insQueue.commands) {
-        // console.log(insQueue.commands);
-        let cmd = insQueue.commands[i];
-        if (cmd.reads.length >= 2) {
-            // console.log(cmd.write);
-            instructions[i] = new Instruction(cmd.name, cmd.write.name, cmd.reads[0].name, cmd.reads[1].name);
-        } else {
-            instructions[i] = new Instruction(cmd.name, cmd.write.name, cmd.reads[0].name, "");
-        }
-    }
-    return instructions;
-}
-function getReservationStations() {
-    let rss = [];
-    for (var i in allRS) {
-        let rs = allRS[i];
-        if (rs.busy() && rs.command.location === "ReservationStation") {
-            rss.push(new ReservationStation(null, rs.name, rs.busy(), rs.command.name, null, null, null, null));
-        } else {
-            rss.push(new ReservationStation(null, rs.name, rs.busy(), "", null, null, null, null));
-        }
-    }
-    return rss;
-}
-
 function run_step_one(callback) {
     Info("one step");
     insQueue.tic();
@@ -54,8 +26,53 @@ function run_step_one(callback) {
     if (hasValue(callback)) {
         callback();
     }
+}
+
+function run_steps(n) {
+    for (var i = 0; i < n; i++) {
+        run_step_one();
+    }
 
 }
+
+//add command(s) split with '\n'
+function addCommandText(text) {
+    insQueue.addCommandsText(text);
+}
+
+
+// ---------- Instruction ----------
+function getInstructions() {
+    let instructions = new Array(insQueue.commands.length);
+    for (var i in insQueue.commands) {
+        // console.log(insQueue.commands);
+        let cmd = insQueue.commands[i];
+        if (cmd.reads.length >= 2) {
+            // console.log(cmd.write);
+            instructions[i] = new Instruction(cmd.name, cmd.write.name, cmd.reads[0].name, cmd.reads[1].name);
+        } else {
+            instructions[i] = new Instruction(cmd.name, cmd.write.name, cmd.reads[0].name, "");
+        }
+    }
+    return instructions;
+}
+
+// ---------- ReservationStation -----------
+function getReservationStations() {
+    let rss = [];
+    for (var i in allRS) {
+        let rs = allRS[i];
+        if (rs.busy() && rs.command.location === "ReservationStation") {
+            rss.push(new ReservationStation(null, rs.name, rs.busy(), rs.command.name, null, null, null, null));
+        } else {
+            rss.push(new ReservationStation(null, rs.name, rs.busy(), "", null, null, null, null));
+        }
+    }
+    return rss;
+}
+
+
+// -------------- registers -------------------
 
 function setRegisterValue(id, val) {
     register_[id].val = val;
@@ -64,6 +81,11 @@ function getRegisterValue(id) {
     return register_[id].val;
 }
 
+function addRegisterValue(id, val) {
+    register_[id].val += val;
+}
+// ----------------- memory ---------------
+
 function setMemValue(id, val) {
     addr_[id].val = val;
 }
@@ -71,4 +93,7 @@ function getMemValue(id) {
     return addr_[id].val;
 }
 
+function addMemValue(id, val) {
+    addr_[id].val += val;
+}
 
