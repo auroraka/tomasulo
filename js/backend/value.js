4b91426b47d6167e31cd206b0a19bf17153e073e
Auroraka <yangtianlong111@gmail.com>
2017-06-07 23:56:39 +0800

    fix calc rs

--- js/backend/value.js
+++ js/backend/value.js
@@ -8,7 +8,6 @@ let VALUE_ID = 233;
 class Value_ {
     constructor(val = 0) {
         this.val = val;
-        this.ready = true;
         this.name = null;
         this.id = VALUE_ID;
         VALUE_ID += 1;
